dbba0670d2d2c5b7d5d3c24ffbe6505e799e314e
Vaal <cedillo.valeria@gmail.com>
2018-10-29 20:46:02 +0100

    conversion

--- src/SparGUI.java
+++ src/SparGUI.java
@@ -1,2 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+
+
 public class SparGUI {
+    private JTextField dimensionality;
+    private JTextField peerID;
+    private JButton saveButton;
+    private JTextPane textPane1;
+    private JTextField vectorValue;
+    private JButton retrieveButton;
+    private JPanel panelMain;
+    private JButton connectButton;
+
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("SparGUI");
+        frame.setPreferredSize(new Dimension(600, 400));
+        frame.setContentPane(new SparGUI().panelMain);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
---
+++ src/Test.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.*;
+
+    public class Test {
+        public static void main(String[] args) {
+
+            String s = "1 1 1 0 0 1 0 1 0 0 0 0 0 1 1 1 1 1 0 1 0 0 0 1 1 0 1 1 1 0";
+            System.out.println(string2int(s));
+            System.out.print(int2string(string2int(s)));
+            //int[] result = string2int(s);
+        }
+
+        public static ArrayList<Integer> string2int(String s) {
+
+            ArrayList<Integer> result = new ArrayList<Integer>();
+            // Splits each spaced integer into a String array.
+            String[] integerStrings = s.split(" ");
+            for (int i = 0; i < integerStrings.length; i++) {
+                result.add(Integer.parseInt(integerStrings[i]));
+            }
+            return result;
+
+        }
+
+        public static String int2string (ArrayList<Integer> inp){
+
+            StringBuilder sb = new StringBuilder("");
+            for (int i = 0; i < inp.size(); i++) {
+                sb.append(inp.get(i)) ;
+
+                if(inp.size()==i-1){
+                    break;
+                }
+                sb.append(" ");
+            }
+            return sb.toString();
+        }
+
+    }
+
+
